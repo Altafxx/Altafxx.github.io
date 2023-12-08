cf3ac78b6735ba7c467909d64e7fb1dca7d36191
altafxx <altafhasnan@gmail.com>
2023-12-08 16:59:50 +0800

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async exportPathMap(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+        return {
+            '/': { page: '/' },
+        };
+    },
+}
 
-module.exports = nextConfig
+export default nextConfig
