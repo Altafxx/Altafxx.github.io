dc301ec38164368e099d04d06e9464861f2e2984
altafxx <altafhasnan@gmail.com>
2024-01-09 12:28:25 +0800

    css improvement

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 interface BannerProps {
     name: string;
     position: string;
@@ -21,21 +22,29 @@ export default function Banner({ name, position, social }: BannerProps) {
     return (
         <div className="text-center md:text-start grid grid-cols-1 md:grid-cols-2">
             <div>
-                <h1>{name}</h1>
-                <h2>{position}</h2>
+                {/* TODO: On hover text gradient animate */}
+                <Link href={"/"}>
+                    <h1 className="bg-gradient-to-r from-purple-500 to-teal-300 bg-clip-text hover:text-transparent transition-all">{name}</h1>
+                </Link>
+                <Link href={"https://en.wikipedia.org/wiki/Software_engineering"}>
+                    <h2 className="text-white/80 text-md hover:-skew-x-12">{position}</h2>
+                </Link>
+                {/* <div className="transition-all bg-animation"></div> */}
             </div>
             <div className="flex overflow-x-auto items-center justify-center gap-4 md:justify-end my-4">
                 {
+                    // TODO: Pulsing 
                     social.map((item, index) => (
-                        <a key={index} href={item.link.join("")}>
+                        <Link key={index} href={item.link.join("")}>
                             <Image
                                 priority
                                 src={logo[index]}
                                 height={48}
                                 width={48}
                                 alt={item.name}
+                                className={"fill-slate-500"}
                             />
-                        </a>
+                        </Link>
                     ))}
             </div>
         </div>
--- src/components/stacks.tsx
+++ src/components/stacks.tsx
@@ -1,78 +1,141 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Stacks() {
+    const imageClass = "hover:scale-110 duration-150 opacity-80 hover:opacity-100 hover:rotate-12 transition-all"
+
     return (
-        <div className="text-center my-6">
+        <div className="text-center my-3 py-3 bg-slate-700/30 rounded-lg ring-2 ring-purple-800/50 shadow-lg shadow-purple-500">
             <h1 className="my-2">Primary Toolbelt</h1>
-            <div className="grid grid-cols-3 md:grid-cols-4 xl:grid-cols-7">
-                <div className="flex flex-col items-center justify-center col-span-1">
-                    <Image
-                        priority
-                        src="/images/logo/git.svg"
-                        height={64}
-                        width={64}
-                        alt="Git"
-                    />
-                    <span className="text-lg mt-2">Github</span>
+            <div className="grid grid-cols-3 md:grid-cols-4 xl:grid-cols-7 gap-4">
+                <div className="flex flex-col items-center justify-center">
+                    <Link href={"https://git-scm.com/"} >
+                        <div className="relative inline-flex group">
+                            <div
+                                className="absolute transitiona-all duration-1000 opacity-0 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-70 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+                            </div>
+                            <Image
+                                priority
+                                src="/images/logo/git.svg"
+                                height={64}
+                                width={64}
+                                alt="Git"
+                                className={imageClass}
+                            />
+                        </div>
+                    </Link>
+                    <span className="text-lg mt-2">Git</span>
                 </div>
-                <div className="flex flex-col items-center justify-center col-span-1">
-                    <Image
-                        priority
-                        src="/images/logo/vercel.svg"
-                        height={64}
-                        width={64}
-                        alt="Vercel"
-                    />
+                <div className="flex flex-col items-center justify-center">
+                    <Link href={"https://vercel.com/"}>
+                        <div className="relative inline-flex group">
+                            <div
+                                className="absolute transitiona-all duration-1000 opacity-0 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-70 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+                            </div>
+                            <Image
+                                priority
+                                src="/images/logo/vercel.svg"
+                                height={64}
+                                width={64}
+                                alt="Vercel"
+                                className={imageClass}
+                            />
+                        </div>
+                    </Link>
                     <span className="text-lg mt-2">Vercel</span>
                 </div>
-                <div className="flex flex-col items-center justify-center col-span-1">
-                    <Image
-                        priority
-                        src="/images/logo/typescript.svg"
-                        height={64}
-                        width={64}
-                        alt="Typescript"
-                    />
-                    <span className="text-lg mt-2">Typescript</span>
-                </div>
-                <div className="flex flex-col items-center justify-center col-span-1">
-                    <Image
-                        priority
-                        src="/images/logo/tailwind.svg"
-                        height={64}
-                        width={64}
-                        alt="Tailwind CSS"
-                    />
+
+                <div className="flex flex-col items-center justify-center">
+                    <Link href={"https://tailwindcss.com/"}>
+                        <div className="relative inline-flex group">
+                            <div
+                                className="absolute transitiona-all duration-1000 opacity-0 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-70 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+                            </div>
+                            <Image
+                                priority
+                                src="/images/logo/tailwind.svg"
+                                height={64}
+                                width={64}
+                                alt="Tailwind CSS"
+                                className={imageClass}
+                            />
+                        </div>
+                    </Link>
                     <span className="text-lg mt-2">Tailwind CSS</span>
                 </div>
-                <div className="flex flex-col items-center justify-center col-span-1">
-                    <Image
-                        priority
-                        src="/images/logo/shadcnui.svg"
-                        height={64}
-                        width={64}
-                        alt="Shadcn/ui"
-                    />
+
+                <div className="flex flex-col items-center justify-center">
+                    <Link href={"https://www.typescriptlang.org/"}>
+                        <div className="relative inline-flex group">
+                            <div
+                                className="absolute transitiona-all duration-1000 opacity-0 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-70 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+                            </div>
+                            <Image
+                                priority
+                                src="/images/logo/typescript.svg"
+                                height={64}
+                                width={64}
+                                alt="Typescript"
+                                className={imageClass}
+                            />
+                        </div>
+                    </Link>
+                    <span className="text-lg mt-2">Typescript</span>
+                </div>
+
+                <div className="flex flex-col items-center justify-center">
+                    <Link href={"https://ui.shadcn.com/"}>
+                        <div className="relative inline-flex group">
+                            <div
+                                className="absolute transitiona-all duration-1000 opacity-0 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-70 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+                            </div>
+                            <Image
+                                priority
+                                src="/images/logo/shadcnui.svg"
+                                height={64}
+                                width={64}
+                                alt="Shadcn/ui"
+                                className={imageClass}
+                            />
+                        </div>
+                    </Link>
                     <span className="text-lg mt-2">Shadcn/ui</span>
                 </div>
-                <div className="flex flex-col items-center justify-center col-span-1">
-                    <Image
-                        priority
-                        src="/images/logo/prisma.svg"
-                        height={64}
-                        width={64}
-                        alt="Git"
-                    />
+
+                <div className="flex flex-col items-center justify-center">
+                    <Link href={"https://www.prisma.io/"}>
+                        <div className="relative inline-flex group">
+                            <div
+                                className="absolute transitiona-all duration-1000 opacity-0 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-70 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+                            </div>
+                            <Image
+                                priority
+                                src="/images/logo/prisma.svg"
+                                height={64}
+                                width={64}
+                                alt="Prisma"
+                                className={imageClass}
+                            />
+                        </div>
+                    </Link>
                     <span className="text-lg mt-2">Prisma</span>
                 </div>
-                <div className="flex flex-col items-center justify-center col-span-1">
-                    <Image
-                        priority
-                        src="/images/logo/postgresql.svg"
-                        height={64}
-                        width={64}
-                        alt="Git"
-                    />
+                <div className="flex flex-col items-center justify-center">
+                    <Link href={"https://www.postgresql.org/"}>
+                        <div className="relative inline-flex group">
+                            <div
+                                className="absolute transitiona-all duration-1000 opacity-0 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-70 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+                            </div>
+                            <Image
+                                priority
+                                src="/images/logo/postgresql.svg"
+                                height={64}
+                                width={64}
+                                alt="PostgreSQL"
+                                className={imageClass}
+                            />
+                        </div>
+                    </Link>
                     <span className="text-lg mt-2">PostgreSQL</span>
                 </div>
             </div>
