b662fbb06822c8c343ac41f8248e25a8c1bcc490
altafxx <altafhasnan@gmail.com>
2024-01-15 19:27:39 +0800

    css improvement

--- src/components/volunteer.tsx
+++ src/components/volunteer.tsx
@@ -10,7 +10,7 @@ interface VolunteerProps {
 };
 
 export default function Volunteer({ open_source }: VolunteerProps) {
-    return <div className="group text- rounded-lg hover:bg-slate-700/30 hover:ring-2 ring-white/10 p-3 transition-all">
+    return <div className="group text- rounded-lg hover:bg-slate-700/30 hover:ring-2 ring-slate-500/30 p-3 transition-all">
         <Badge className="bg-gradient-to-r group-hover:to-purple-500/50 group-hover:from-teal-300/50 shadow-black shadow-lg group-hover:scale-105 transition-all">Volunteer Work and Interests</Badge>
         {
             open_source.map((item, index) => (
