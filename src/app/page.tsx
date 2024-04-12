f377fdbf2ab08ecb97eeb52818f4196b20dc213e
altafxx <altafhasnan@gmail.com>
2024-04-12 22:46:50 +0800

    v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,112 @@
+"use client"
 import profile from "@/data/profile.json"
-import { Banner, Skills, Experience, Education, Volunteer, Stacks } from "@/components";
-import { Badge } from "@/components/ui/badge";
-import { Card } from "@/components/ui/card";
+import folder from "@/components/v2/folder";
+import { Toolbelt } from "@/components/v2/toolbelt";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+
+
+const close = <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>
+
+const maximize = <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 512 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM96 96H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32s14.3-32 32-32z" /></svg>
+
+const minimize = <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 512 512"><path d="M32 416c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H32z" /></svg>
+
+const doorLogo = <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 576 512"><path d="M96 64c0-35.3 28.7-64 64-64H416c35.3 0 64 28.7 64 64V448h64c17.7 0 32 14.3 32 32s-14.3 32-32 32H432 144 32c-17.7 0-32-14.3-32-32s14.3-32 32-32H96V64zM384 288a32 32 0 1 0 0-64 32 32 0 1 0 0 64z" /></svg>
+
+const useResponsiveWidth = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const responsiveWidth = screenWidth <= 768 ? 100 : screenWidth <= 1024 ? 125 : 200; // Adjust breakpoint as needed
+
+  return responsiveWidth;
+};
 
 export default function Home() {
+  const responsiveWidth = useResponsiveWidth();
+  const [personalInfo, setPersonalInfo] = useState(true);
+
+  const togglePersonalInfo = () => setPersonalInfo(!personalInfo)
+
   return (
-    <main className='py-12 px-8 md:px-12 xl:px-24 flex-1 overflow-y-auto'>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <main className="min-h-screen bg-gradient-to-br from-pink-400 via-purple-400 to-cyan-400">
+      <div className="m-6 p-1 flex justify-between">
         <div>
-          <Card className="p-4">
-            <Badge>Work Experience</Badge>
-            <div className="grid grid-cols-2">
-            </div>
-          </Card>
+          {folder({ name: "Personal Information", action: togglePersonalInfo, state: personalInfo })}
         </div>
-        <div>
-          <Card className="p-4">
-            <Badge>Work Experience</Badge>
-            <div className="grid grid-cols-2">
+        <div className="grid grid-flow-col">
+          <div>
+            {folder({ name: "Experience" })}
+          </div>
+          <div>
+            {folder({ name: "Projects and Interest" })}
+            {folder()}
+            {folder()}
+          </div>
+        </div>
+      </div>
+      {
+        personalInfo ?
+
+          <div className="bg-slate-300 absolute left-10 right-10 sm:left-24 sm:right-24 md:left-30 md:right-30 lg:left-44 lg:right-44 xl:left-52 xl:right-52 2xl:left-72 2xl:right-72 top-32 bottom-40 max-lg:bottom-15 max-xl:bottom-20 rounded-sm shadow-lg">
+            <div className="mt-3">
+              <div className="absolute right-0 left-0 text-center text-black/50 font-medium max-sm:hidden">Personal Information</div>
+              <div className="grid grid-flow-col gap-2 absolute right-0 mr-3">
+                <div className="bg-green-500 w-4 h-4 rounded-full cursor-pointer" />
+                <div className="bg-yellow-500 w-4 h-4 rounded-full cursor-pointer" />
+                <div className="bg-red-500 w-4 h-4 rounded-full cursor-pointer" onClick={() => { togglePersonalInfo() }} />
+              </div>
             </div>
-          </Card>
+            <div>
+              <div className="bg-slate-800 m-3 absolute bottom-0 top-8 left-0 right-0 rounded-sm p-4 text-white/80">
+                <div className="grid grid-cols-4 max-lg:grid-cols-1 justify-items-center max-md:mt-2 max-xl:mt-6 mt-8 gap-4">
+                  <div className=" grid grid-cols-3 lg:grid-cols-1">
+                    <Image
+                      src={"/images/MCSkin.png"}
+                      height={600}
+                      width={responsiveWidth}
+                      alt={"Avatar model"}
+                      className={`transition-all duration-300 flex `}
+                    />
+                    <div className="m-2 col-span-2 lg:hidden">
+                      <div className="text-xl font-semibold text-white/80">{profile.position}</div>
+                      <div className="text-xl max-md:hidden">{profile.name.first} • {profile.location.city}, {profile.location.country}</div>
+                      <div className="md:hidden">
+                        <div className="text-xl">{profile.name.first}</div>
+                        <div className="text-xl">{profile.location.city}, {profile.location.country}</div>
+                      </div>
+                    </div>
+
+                  </div>
+                  {/* <img src="/images/MCSkin.png" className="scale-50"></img> */}
+                  <div className="col-span-3">
+                    <div>
+                      <div className="m-2 max-lg:hidden">
+                        <div className="text-xl font-semibold text-white/80">{profile.position}</div>
+                        <div className="text-xl ">{profile.name.first} • {profile.location.city}, {profile.location.country}</div>
+                      </div>
+                      <Toolbelt />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          : null
+      }
+      <div className=" bg-gradient-to-l from-pink-400/40 via-purple-400/40 to-cyan-400/40 outline outline-white/10  w-full h-14 fixed bottom-0 flex justify-center items-center">
+        <div className="rounded-full bg-white/30 hover:bg-white/50 w-fit p-2 cursor-pointer hover:ring-2 ring-1 ring-black/20 transition-all ease-in-out duration-300">
+          {doorLogo}
         </div>
       </div>
     </main>
-  )
+  );
 }
---
+++ src/components/v2/toolbelt.tsx
@@ -0,0 +1,145 @@
+"use client"
+import * as React from "react"
+import Image from "next/image"
+// import { NodeJS } from 'node';
+
+import { Card, CardContent } from "@/components/ui/card"
+import { useEffect, useState } from "react"
+
+const stacks = [
+    "/images/logo/light/git.svg",
+    "/images/logo/light/vercel.svg",
+    "/images/logo/light/tailwind.svg",
+    "/images/logo/light/typescript.svg",
+    "/images/logo/light/shadcnui.svg",
+    "/images/logo/light/prisma.svg",
+    "/images/logo/light/nextjs.svg",
+    "/images/logo/light/postgresql.svg"
+]
+
+const stackNames = [
+    "Git",
+    "Vercel",
+    "Tailwind",
+    "Typescript",
+    "Shadcn/ui",
+    "Prisma",
+    "Next JS",
+    "Postgresql"
+]
+
+const stackLinks = [
+    "https://git-scm.com/",
+    "https://vercel.com/",
+    "https://tailwindcss.com/",
+    "https://www.typescriptlang.org/",
+    "https://ui.shadcn.com/",
+    "https://www.prisma.io/",
+    "https://nextjs.org/",
+    "https://www.postgresql.org/",
+]
+
+const stackDef = [
+    "Git is a popular version control system for tracking changes in computer files. It's particularly useful for software development projects where multiple people collaborate on the code",
+    "Vercel is a cloud platform designed specifically for the frontend aspects of web development, often referred to as the \"Frontend Cloud\".  In simpler terms, it helps developers deploy and manage the user-facing parts of websites and web applications.",
+    "Tailwind CSS is a CSS framework unlike others. Instead of pre-built styles, it offers basic building blocks (utility classes) that you combine directly in your HTML for quick, custom website design.",
+    "Typescript is a superset of JavaScript that adds static typing to the language. It helps catch errors early in the development process, making your code more reliable and easier to maintain.",
+    "Shadcn/ui offers pre-built buttons, forms, etc. for your web apps. Just copy and paste! Save time, customize freely, and build accessible apps.",
+    "Prisma simplifies talking to your database. Define your data, write safe queries, and manage changes easily. It acts like a translator between your code and the database, making data management smooth.",
+    "Next.js is a React framework that enables server-side rendering and static site generation for React applications. It allows you to build web applications with speed and efficiency, providing a great developer experience.",
+    "PostgreSQL is a powerful, open-source object-relational database system. It's known for its reliability, robustness, and performance, making it a popular choice for many applications.",
+]
+
+const useResponsiveWidth = () => {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => setScreenWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const responsiveWidth = screenWidth <= 768 ? 42 : screenWidth <= 1024 ? 58 : 88; // Adjust breakpoint as needed
+
+    return responsiveWidth;
+};
+export function Toolbelt() {
+    const [current, setCurrent] = React.useState(0)
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrent(current => (current + 1) % stacks.length);
+        }, 2000);
+        return () => clearInterval(interval);
+    }, []);
+
+
+    function clickFunc(index: number) {
+        if (current === index) {
+            var mouseClick = new Audio("sounds/mouse-click.mp3");
+            mouseClick.play();
+            return;
+        }
+
+        setCurrent(index);
+        var mouseClick = new Audio("sounds/bubble.mp3");
+        mouseClick.play();
+    }
+
+
+    return (
+        <Card>
+            <CardContent className="p-6">
+                <div className="flex flex-col xl:flex-row gap-4">
+                    <div className="flex-2">
+                        <div className="grid grid-cols-2 max-xl:grid-cols-4 max-lg:grid-cols-4 gap-4">
+                            {Array.from(stacks).map((_, index) => {
+                                return (
+                                    <div key={index} className={`m-2 p-2 rounded-sm bg-gradient-to-tl from-pink-400/40 via-purple-400/40 to-cyan-400/40 w-fit self-center select-none cursor-pointer hover:from-pink-400/60 group hover:shadow-md ring ${index == current ? "ring-2 ring-pink-400" : "ring-0"} transition-all duration-300 ease-in-out`} onClick={() => clickFunc(index)}>
+                                        <Image
+                                            priority
+                                            src={stacks[index]}
+                                            height={24}
+                                            width={24}
+                                            alt={stackNames[index]}
+                                            className={`group-hover:rotate-12 group-hover:animate-tilt transition-all  duration-300 ${index == current ? "scale-125" : ""}`}
+                                        />
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                    <div className="self-center text-center">
+                        <Card className="px-14 py-16 max-md:px-24 max-md:py-10 max-xl:px-24 max-xl:py-10 select-none shadow-md">
+                            <Image
+                                key={current}
+                                priority
+                                src={stacks[current]}
+                                height={useResponsiveWidth()}
+                                width={useResponsiveWidth()}
+                                alt="Logo"
+                                className="transition-all duration-300 ease-in-out"
+                            />
+                            <div className="mt-2 font-medium">{stackNames[current]}</div>
+                        </Card>
+                    </div>
+                    <div className="lg:flex-1 h-full flex flex-col justify-between">
+                        <div className="grid grid-flow-row my-2 xl:my-4">
+                            <div>
+                                <div className="text-lg font-semibold">{stackNames[current]}</div>
+                                <div>
+                                    {stackDef[current]}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex flex-col justify-end">  <div className="">
+                            <span>Product Site:</span>
+                            <a href={stackLinks[current]} className="ml-2 text-blue-500">{stackLinks[current]}</a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
