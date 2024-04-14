48183ae35b28a3e5e3ab49eac889c8aa4b9bbc60
altafxx <altafhasnan@gmail.com>
2024-04-14 18:54:28 +0800

    v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const minimize = <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" 
 const doorLogo = <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 576 512"><path d="M96 64c0-35.3 28.7-64 64-64H416c35.3 0 64 28.7 64 64V448h64c17.7 0 32 14.3 32 32s-14.3 32-32 32H432 144 32c-17.7 0-32-14.3-32-32s14.3-32 32-32H96V64zM384 288a32 32 0 1 0 0-64 32 32 0 1 0 0 64z" /></svg>
 
 const calculateResponsiveWidth = (width: number) => {
-  return width <= 768 ? 70 : width <= 1024 ? 125 : 200;
+  return width <= 768 ? 50 : width <= 1024 ? 100 : 200;
 };
 
 const useResponsiveWidth = () => {
@@ -78,7 +78,7 @@ export default function Home() {
               </div>
               <div>
                 <div className="bg-slate-800 m-3 absolute bottom-0 top-8 left-0 right-0 rounded-sm p-4 text-white/80">
-                  <div className="grid grid-cols-4 max-lg:grid-cols-1 justify-items-center max-md:mt-2 max-xl:mt-6 mt-8 gap-4">
+                  <div className="grid grid-cols-4 max-lg:grid-cols-1 justify-items-center max-md:mt-0 max-xl:mt-6 mt-8 gap-4">
                     <div className=" grid grid-cols-3 lg:grid-cols-1">
                       <Image
                         src={"/images/MCSkin.png"}
