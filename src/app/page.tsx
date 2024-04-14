49199d6b520d4ea8c0420dc0a8f0f7b8b5d28113
altafxx <altafhasnan@gmail.com>
2024-04-14 21:36:26 +0800

    v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Folder from "@/components/v2/folder";
 import { Toolbelt } from "@/components/v2/toolbelt";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import AvatarModel from "@/components/v2/avatarmodel";
 
 
 
@@ -15,31 +16,7 @@ const minimize = <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" 
 
 const doorLogo = <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 576 512"><path d="M96 64c0-35.3 28.7-64 64-64H416c35.3 0 64 28.7 64 64V448h64c17.7 0 32 14.3 32 32s-14.3 32-32 32H432 144 32c-17.7 0-32-14.3-32-32s14.3-32 32-32H96V64zM384 288a32 32 0 1 0 0-64 32 32 0 1 0 0 64z" /></svg>
 
-const calculateResponsiveWidth = (width: number) => {
-  return width <= 768 ? 50 : width <= 1024 ? 100 : 200;
-};
-
-const useResponsiveWidth = () => {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const [responsiveWidth, setResponsiveWidth] = useState(calculateResponsiveWidth(screenWidth));
-
-  useEffect(() => {
-    const handleResize = () => {
-      setScreenWidth(window.innerWidth);
-      setResponsiveWidth(calculateResponsiveWidth(window.innerWidth));
-    }
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  // const responsiveWidth = screenWidth <= 768 ? 70 : screenWidth <= 1024 ? 125 : 200; // Adjust breakpoint as needed
-
-  return responsiveWidth;
-};
-
 export default function Home() {
-  const responsiveWidth = useResponsiveWidth();
   const [personalInfo, setPersonalInfo] = useState(true);
 
   const togglePersonalInfo = () => setPersonalInfo(!personalInfo)
@@ -65,7 +42,7 @@ export default function Home() {
         {
           personalInfo ?
 
-            <div className="bg-slate-300 absolute left-10 right-10 sm:left-24 sm:right-24 md:left-30 md:right-30 lg:left-44 lg:right-44 xl:left-52 xl:right-52 2xl:left-72 2xl:right-72 top-32 bottom-40 max-lg:bottom-15 max-xl:bottom-20 rounded-sm shadow-lg">
+            <div className="bg-slate-300 absolute left-6 right-6 sm:left-20 sm:right-20 md:left-30 md:right-30 lg:left-44 lg:right-44 xl:left-52 xl:right-52 2xl:left-72 2xl:right-72 top-10 md:top-32 bottom-40 max-lg:bottom-15 max-xl:bottom-20 rounded-sm shadow-lg">
               <div className="mt-2 md:mt-3">
                 <div className="absolute right-0 left-0 text-center text-black/50 font-medium text-sm md:text-base max-sm:hidden">Personal Information</div>
                 <div className="grid grid-flow-col gap-2 absolute right-0 mr-3 pt-0 sm:pt-1">
@@ -75,23 +52,16 @@ export default function Home() {
                 </div>
               </div>
               <div>
-                <div className="bg-slate-800 m-3 absolute bottom-0 top-4 sm:top-6 md:top-8 left-0 right-0 rounded-sm p-4 text-white/80">
+                <div className="bg-slate-800 m-2 mt-3 md:m-3 absolute bottom-0 top-4 sm:top-6 md:top-8 left-0 right-0 rounded-sm p-4 text-white/80">
                   <div className="grid grid-cols-4 max-lg:grid-cols-1 justify-items-center max-md:mt-0 max-xl:mt-6 mt-8 gap-4">
                     <div className=" grid grid-cols-3 lg:grid-cols-1">
-                      <Image
-                        src={"/images/MCSkin.png"}
-                        height={100}
-                        width={responsiveWidth}
-                        alt={"Avatar model"}
-                        className={`transition-all duration-700 flex `}
-                      />
-                      {/* {responsiveWidth ? <div>Loaded {responsiveWidth}</div> : <div>Loading {responsiveWidth}</div>} */}
+                      <AvatarModel />
                       <div className="m-2 col-span-2 lg:hidden">
                         <div className="text-sm lg:text-xl font-semibold text-white/80">{profile.position}</div>
                         <div className="text-xl max-md:hidden">{profile.name.first} • {profile.location.city}, {profile.location.country}</div>
                         <div className="md:hidden">
                           <div className="text-xl">{profile.name.first}</div>
-                          <div className="text-sm">{profile.location.city}, {profile.location.country}</div>
+                          <div className="text-xs">{profile.location.city}, {profile.location.country}</div>
                         </div>
                       </div>
                     </div>
