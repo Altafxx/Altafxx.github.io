5b6c14a2250c3cddda7a45fe24b2c4010c46ae16
altafxx <altafhasnan@gmail.com>
2024-04-14 22:18:57 +0800

    implement zustand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client"
 import profile from "@/data/profile.json"
 import Folder from "@/components/v2/folder";
-import { Toolbelt } from "@/components/v2/toolbelt";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import Toolbelt from "@/components/v2/toolbelt";
 import AvatarModel from "@/components/v2/avatarmodel";
-
-
+import { useV2 } from "@/provider/v2_provider";
 
 const close = <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>
 
@@ -17,11 +14,10 @@ const minimize = <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" 
 const doorLogo = <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 576 512"><path d="M96 64c0-35.3 28.7-64 64-64H416c35.3 0 64 28.7 64 64V448h64c17.7 0 32 14.3 32 32s-14.3 32-32 32H432 144 32c-17.7 0-32-14.3-32-32s14.3-32 32-32H96V64zM384 288a32 32 0 1 0 0-64 32 32 0 1 0 0 64z" /></svg>
 
 export default function Home() {
-  const [personalInfo, setPersonalInfo] = useState(true);
+  // const [personalInfo, setPersonalInfo] = useState(true);
+  const { profileState, setProfileState } = useV2()
 
-  const togglePersonalInfo = () => setPersonalInfo(!personalInfo)
-  useEffect(() => { }, [personalInfo])
-
+  const togglePersonalInfo = () => setProfileState(!profileState)
   function closePage(action?: Function) {
     var mouseClick = new Audio("sounds/mouse-click.mp3");
     mouseClick.play();
@@ -35,7 +31,7 @@ export default function Home() {
       <div className="min-h-screen bg-gradient-to-br from-pink-400 via-purple-400 to-cyan-400">
         <div className="mx-6 p-1 flex justify-between">
           <div>
-            {Folder({ name: "Personal Information", action: togglePersonalInfo, state: personalInfo })}
+            {Folder({ name: "Personal Information", action: togglePersonalInfo, state: profileState })}
           </div>
           <div className="grid grid-flow-col">
             <div>
@@ -49,7 +45,7 @@ export default function Home() {
           </div>
         </div>
         {
-          personalInfo ?
+          profileState ?
 
             <div className="bg-slate-300 absolute left-6 right-6 sm:left-20 sm:right-20 md:left-30 md:right-30 lg:left-44 lg:right-44 xl:left-52 xl:right-52 2xl:left-72 2xl:right-72 top-10 md:top-32 bottom-40 max-lg:bottom-15 max-xl:bottom-20 rounded-sm shadow-lg">
               <div className="mt-2 md:mt-3">
