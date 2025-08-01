1b8c626bc0a4a3389f8ff3e9a44bcf133d74c89a
altaf <altafhasnan@gmail.com>
2025-08-01 17:03:26 +0800

    fix: v2 layout

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: { unoptimized: true }
+    // output: 'export',
+    images: { unoptimized: true },
+    rewrites: () => {
+        return [
+            {
+                source: '/',
+                destination: '/v2',
+            },
+        ]
+    }
 }
 
 module.exports = nextConfig
--- src/provider/v2_provider.tsx
+++ src/provider/v2_provider.tsx
@@ -2,6 +2,32 @@ import { create } from 'zustand'
 
 export const useV2 = create((set: any, get: any) => ({
     profileState: false,
-    setProfileState: (profileState: any) => set(() => ({ profileState })),
+    experienceState: false,
+    projectsState: false,
+    educationState: false,
+    setProfileState: (profileState: any) => set(() => ({
+        profileState,
+        experienceState: profileState ? false : get().experienceState,
+        projectsState: profileState ? false : get().projectsState,
+        educationState: profileState ? false : get().educationState
+    })),
+    setExperienceState: (experienceState: any) => set(() => ({
+        experienceState,
+        profileState: experienceState ? false : get().profileState,
+        projectsState: experienceState ? false : get().projectsState,
+        educationState: experienceState ? false : get().educationState
+    })),
+    setProjectsState: (projectsState: any) => set(() => ({
+        projectsState,
+        profileState: projectsState ? false : get().profileState,
+        experienceState: projectsState ? false : get().experienceState,
+        educationState: projectsState ? false : get().educationState
+    })),
+    setEducationState: (educationState: any) => set(() => ({
+        educationState,
+        profileState: educationState ? false : get().profileState,
+        experienceState: educationState ? false : get().experienceState,
+        projectsState: educationState ? false : get().projectsState
+    })),
     // getCode: () => (get().profile),
 }))
